aca1df2065d383abbb39a56a824dc17343b3759d
Habinezajanvier <habinezajanvier688@gmail.com>
2020-08-12 13:22:58 +0200

    chore(mantain): set node environment for local development

---
+++ src/models/article.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const article = mongoose.model('Article', articleSchema);
+export default article;
---
+++ src/models/connection.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import config from '../../config/config';
+
+const {
+  db: { username, password, host, port, name },
+} = config;
+
+const url =
+  `mongodb://${username}:${password}@${host}:${port}/${name}?authSource=admin` ||
+  config.db.database_url;
+
+const options = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+export default () => {
+  mongoose
+    .connect(url, options)
+    .then(() => console.log('connected to mongodb'))
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
